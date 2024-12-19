4f88424c285a113da06caba82b12ddde375f044d
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-19 18:22:04 +0100

    feat(steps): add the steps array

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header({ step }) {
+    return (
+        <header>
+            
+        </header>
+    )
+}
---
+++ src/components/form/steps.js
@@ -0,0 +1,29 @@
+// Will need to import the step components here
+
+export const steps = [
+  {
+    stepId: 0,
+    stepName: "Personal details",
+    component: PersonalDetailsStep,
+  },
+  {
+    stepId: 1,
+    stepName: "Bio",
+    component: BioStep,
+  },
+  {
+    stepId: 2,
+    stepName: "WorkExperience",
+    component: WorkExperienceStep,
+  },
+  {
+    stepId: 3,
+    stepName: "Projects",
+    component: ProjectsStep,
+  },
+  {
+    stepId: 4,
+    stepName: "Education",
+    component: EducationStep,
+  },
+];
