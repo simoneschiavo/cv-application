ae4f2d1fe3f6b348121f40a28021bc50d4685c0d
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-19 19:07:22 +0100

    feat(App): initialize formData

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import { v4 as uuidv4 } from 'uuid'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [currentStep, setCurrentStep] = useState(0)
+  const [currentStep, setCurrentStep] = useState(0);
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    jobTitle: '',
-  })
+    firstName: "",
+    lastName: "",
+    jobTitle: "",
+    email: "",
+    linkedinProfile: "",
+    address: "",
+    phoneNumber: "",
+    bio: "",
+    workExperience: [
+      {
+        id: uuidv4(),
+        jobTitle: "",
+        companyName: "",
+        workLocation: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ],
+    projects: [
+      {
+        id: uuidv4(),
+        title: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ],
+    education: [
+      {
+        id: uuidv4(),
+        institutionName: "",
+        startDate: "",
+        endDate: "",
+        name: "",
+      },
+    ],
+  });
 
   return (
     <>
@@ -18,7 +52,7 @@ function App() {
       Buttons (back, next, submit)
       <Resume formData = { formData } /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
