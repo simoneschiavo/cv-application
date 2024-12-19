82f135ff337de785cc48396d8a7666d6312ae30a
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-19 18:23:57 +0100

    refactor(App): useState for currentStep instead of step

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 // function App() {
-//   const [step, setStep] = useState(0)
+//   const [currentStep, setCurrentStep] = useState(0)
 //   const [formData, setFormData] = useState({
 //     firstName: '',
 //     lastName: '',
