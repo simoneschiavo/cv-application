f914aa31608edf3ada49d4d03ef55f4df51bad49
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-19 18:32:48 +0100

    feat(Header): add steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
-// function App() {
-//   const [currentStep, setCurrentStep] = useState(0)
-//   const [formData, setFormData] = useState({
-//     firstName: '',
-//     lastName: '',
-//     jobTitle: '',
-//   })
+function App() {
+  const [currentStep, setCurrentStep] = useState(0)
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    jobTitle: '',
+  })
 
-//   return (
-//     <>
-//       <Header step={step} />
-//       Form Steps
-//       Buttons (back, next, submit)
-//       <Resume formData = { formData } />
-//     </>
-//   )
-// }
+  return (
+    <>
+      <Header currentStep={currentStep} />
+      {/* Form Steps
+      Buttons (back, next, submit)
+      <Resume formData = { formData } /> */}
+    </>
+  )
+}
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-export default function Header({ step }) {
-    return (
-        <header>
-            
-        </header>
-    )
+import { steps } from "./form/steps";
+
+export default function Header({ currentStep }) {
+  return (
+    <header>
+      {steps.map((step, index) => (
+        <div
+          key={step.stepId}
+          className={
+            currentStep === index ? "header-current-step" : "header-step"
+          }
+        >
+          {step.stepName}
+        </div>
+      ))}
+    </header>
+  );
 }
