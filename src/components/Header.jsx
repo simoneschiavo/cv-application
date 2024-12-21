7b20b675763609fd0a109322625247bd298634df
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-21 15:14:33 +0100

    feat(App.css): add frosted glass effect to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import { steps } from "./form/steps";
 export default function Header({ currentStep }) {
   return (
     <header>
+      <section className="backdrop"></section>
       {steps.map((step, index) => (
         <div
           key={step.stepId}
           className={
-            currentStep === index ? "header-current-step" : "header-step"
+            currentStep === index ? "header-current-step header-step" : "header-step"
           }
         >
           {step.stepName}
         </div>
       ))}
+      <section className="backdrop-edge"></section>
     </header>
   );
 }
